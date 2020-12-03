5839e8187e58e2dc07ff7fce2384b67ab54e29ff
arukil <arukilt20@gmail.com>
2020-12-03 17:31:33 +0530

    fix

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 const router = require('./router');
 
 const app = express();
+
+
 const server = http.createServer(app);
 const io = socketio(server);
 
@@ -16,24 +18,21 @@ app.use(router);
 
 
 io.on('connect', (socket) => {
-  //console.log(socket)
+
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
     if(error) return callback(error);
 
     socket.join(user.room);
-
     socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
-
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-
     callback();
+    
   });
 
   socket.on('sendMessage', (message, callback) => {
-    
     
     const user = getUser(socket.id );
      if(!user)
@@ -42,7 +41,7 @@ io.on('connect', (socket) => {
      }
     
     io.to(user.room).emit('message', { user: user.name, text: message });
-
+ 
     callback();
   });
 
@@ -56,4 +55,6 @@ io.on('connect', (socket) => {
   })
 });
 
-server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
+server.listen( process.env.PORT ||3000, () => console.log(`Server has started.`));
+
+
